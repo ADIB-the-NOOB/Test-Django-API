b630f7b3b1b58feadd111d8b70a40355edab8bce
ADIB-the-NOOB <mdadib550@gmail.com>
2023-04-27 22:16:58 +0600

    Host Added

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hqey19pzeysnc*+=b(vrwk&$=&g3_$t8e=)qrvufq35@e%=#wm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
